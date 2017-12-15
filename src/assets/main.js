e4c11337083713710a26b4bf998d286e8dc6f30b
Chen Sharon <chens@traiana.com>
2017-12-15 15:23:07 -0500

    update

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,17 +1,36 @@
 let answer = document.getElementById('answer');
 let attempt = document.getElementById('attempt');
+let message = document.getElementById('message');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if (input.value === '') {
+        setHiddenFields();
+    }
+    if (!validateInput(input.value)) {
+        return false;
+    } else {
+        attempt.value++;
+    }
+
 }
 
 function setHiddenFields() {
     const zeros = ['', '0', '00', '000', '0000'];
     const guess = Math.floor(Math.random() * 10000);
     const add = 4 - guess.toString().length;
-    answer.value = zeros[add] + guess.toString();
-    attempt.value = 0;
+    answer.value = answer.value || zeros[add] + guess.toString();
+    attempt.value = attempt.value || 0;
 }
 
-setHiddenFields();
+function setMessage(msg) {
+    message.innerHTML = msg;
+}
+
+function validateInput(input) {
+    if (input.length !== 4) {
+        setMessage('Guesses must be exactly 4 characters long.');
+        return false;
+    }
+    return true;
+}
